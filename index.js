03e07add08d240a4ae1a5918e2fba52b234e9991
BrandonBrown <brandonbrown@pursuit.org>
2019-11-09 14:36:55 -0500

    question 2

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 document.addEventListener("DOMContentLoaded",() => {
 
-let stringInput = prompt("What's your name?")
+// let stringInput = prompt("What's your name?")
 
-alert("Hello " + stringInput)
-document.getElementById("stringName").innerText = "Hello " + stringInput
+// alert("Hello " + stringInput)
+// document.getElementById("stringName").innerText = "Hello " + stringInput
+
+
+let numberInput = prompt("What's your age")
+let output = ""
+if (numberInput % 2 === 0){
+    alert("Your age even")
+    output ="Your age is is even"
+    document.getElementById("age").innerText = output 
+} else {
+    alert("Your age is not even")
+    output = "Your age is not even"
+    document.getElementById("age").innerText = output 
+}
+
+
 
 
 
