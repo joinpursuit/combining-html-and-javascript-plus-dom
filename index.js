01f077acc22bbc36b1487d6aae541f6dcddf2cb6
bsihame <sihamebazi@pursuit.org>
2019-11-09 17:32:38 -0500

    solve problem 3 c

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
   document.getElementById("submit").addEventListener("click",()=>{
   
   num = document.getElementById("number").value
+  // newNum = document.getElementById("number")
 
   let output = Number(num) * 2;
   // alert(output)
-  let newOutput = document.getElementById("doubled")
-  newOutput.innerText = output
-  //debugger
- 
-   
+  
+  // let newOutput = document.getElementById("doubled")
+  
+  //newOutput.innerText = output
+  number.value = output
   
 })
 })
