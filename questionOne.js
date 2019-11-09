588f0a759028e1c90df091ac076e16a0b24d149a
Danielle R Cherry <daniellecherry@pursuit.org>
2019-11-09 14:54:45 -0500

    Complete Question One

---
+++ questionOne.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    let enterName = window.prompt("enter")
+
+    let result = alert(`${enterName}`);
+    document.querySelector("#name").innerText = string
+
+})
