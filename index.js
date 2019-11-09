78be31e62c431749c22cee5a6cbc416f3bfac659
Kevin Wong <kevinwong@pursuit.org>
2019-11-09 16:17:34 -0500

    question 3 completed

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     
+    // question 1
     // let q1 = prompt("Enter a string:")
     // // alert(q1);
     // let header = document.querySelector("#q1")
     // header.innerText = q1;
 
 
-
+    //question 2
     // let q2 = prompt("Enter a number:")
     // let header2 = document.querySelector("#q2");
     // if(q2 % 2 === 0){
@@ -16,13 +17,25 @@ document.addEventListener("DOMContentLoaded", () => {
     //     // alert("Number is NOT EVEN");
     //     header2.innerText = "Number is NOT EVEN";
     // }
-
-    let number = document.getElementById("#q3");
-    if(document.querySelector("#x2")){
-        
-    }
-    
-    
     debugger
 
 })
+
+// question 3
+const TimesTwo = () => {
+  document.querySelector("#q3").innerText = num.value * 2;
+  num.value = document.querySelector("#q3").innerText
+}
+
+//question 4
+const isPalindrome = () => {
+    let userInput = document.querySelector("#palInput").innerText
+    let result = document.querySelector("#resultPal")
+    for(let i = 0; i < Math.floor(userInput.length/2); i++){
+        if(userInput[userInput.length - 1 - i] !== userInput[i]){
+            result.innerText = "FALSE";
+            
+        } 
+    }
+    result.innerText = "TRUE";
+}
