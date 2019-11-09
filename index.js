d2e75c61733d10c8957530729fc614e9a0956005
bsihame <sihamebazi@pursuit.org>
2019-11-09 18:09:41 -0500

    solve problem 4

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
   document.addEventListener("DOMContentLoaded",()=>{
-  document.getElementById("submit").addEventListener("click",()=>{
+    document.getElementById("submit").addEventListener("click",()=>{
+      
+      str = document.getElementById("string").value
+      // newStr = document.getElementById("string")
+    
+      
+  let newStr = ""
+  for(let i = str.length-1; i >=0; i--){
+    newStr +=str[i]
+  }
   
-  num = document.getElementById("number").value
-  // newNum = document.getElementById("number")
-
-  let output = Number(num) * 2;
+  
+  let string = document.getElementById("check")
+  let output2;
+  
+  if(newStr === str){
+    //alert("palindrome found")
+     output2 = "palindrome found"
+  } else{
+    //alert("not a palindrome")
+     output2 = "palindrome found"
+  }
+  string.innerText = output2
+  
+    
+  
+    
   // alert(output)
-  
-  // let newOutput = document.getElementById("doubled")
-  
-  //newOutput.innerText = output
-  number.value = output
-  
+//   number.value = output
+debugger
+
 })
 })
