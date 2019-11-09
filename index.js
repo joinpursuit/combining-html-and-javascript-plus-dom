1bc60e203989d41097a75a411cac0620ad3e2a27
BrandonBrown <brandonbrown@pursuit.org>
2019-11-09 14:20:44 -0500

    Question 1

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded",() => {
+
+let stringInput = prompt("What's your name?")
+
+alert("Hello " + stringInput)
+document.getElementById("stringName").innerText = "Hello " + stringInput
+
+
+
+})
