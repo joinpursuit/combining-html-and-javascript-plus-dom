c0e730cb382dd6aeb49d1341235eb03853048e00
Farrah Isabelle <farrahrios@pursuit.org>
2019-11-09 17:06:26 -0500

    up to num 4

---
+++ index2.js
@@ -0,0 +1,43 @@
+// a. Make a website that prompts the user for a string. Then show a popup that displays the string.
+document.addEventListener("DOMContentLoaded",()=> {
+    let newName =(prompt("What is your name"))
+ let alertOne = (alert(newName))
+    alertOne.innerText = newName
+
+
+})
+
+// b. Instead of showing a popup, make the text appear on the screen.
+document.addEventListener("DOMContentLoaded",()=> {
+
+    let newPassion =(prompt("What is your Passion"))
+    let passionTag = document.querySelector("#Cooking");
+   passionTag.innerText = newPassion
+
+})
+
+// a. Make a website that prompts the user for a number, then alerts them of whether or not the number is even.
+document.addEventListener("DOMContentLoaded",()=> {
+
+    let number =(prompt("give me a number and make it even"))
+    //b. Instead of showing a popup, make the text appear on the screen.
+    let numTag = document.querySelector("#num");
+    numTag.innerText = number
+ const isEven = (num) => {
+     if (num %2 === 0)return true;
+     if (num %2 !== 0)return false;
+ }
+if (isEven(Number(number))){
+    alert ("your numbern is even")
+} else {
+    alert("you are an odd fellow ")
+}
+    })
+
+
+
+    
+
+    // a. Make a website with an input field and a button. 
+    //  Have a heading at the top that reads "Double your number below"  
+    //  When the user clicks on the button, display an alert showing that number times two.
