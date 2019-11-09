f181f4eb815aa7dfccf5233a8718953a971fba85
darsuabasi <uduakabasiabasiurua@pursuit.org>
2019-11-09 14:49:11 -0500

    finished q1

---
+++ java1.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let string = prompt("What is your name?") 
+    let pop = alert(`${string}`)
+    document.querySelector("#q1b-string").innerText = innerText = string
+
+})
+
+
+
