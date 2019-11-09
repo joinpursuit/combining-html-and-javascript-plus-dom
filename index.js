8083a82451b7952b951b5a5685e678f5053acd76
Dugmar <dugmar.morocho@gmail.com>
2019-11-09 18:00:25 -0500

    problem number 8

--- index.js
+++ index.js
@@ -24,7 +24,7 @@
 //     document.querySelector("#num").value = `${num.value} Times Two Is ${Number(num.value)*2}`
 // }
 
-//Problem 4
+// Problem 4
 // function pallindrome(){
 //     let str = document.querySelector("#arr").value
 //     for(let i = 0; i < Math.floor(str.length/2); i++){
@@ -42,3 +42,54 @@
 //     let str = document.querySelector("#str")
 //     string2.innerText = `${str.value.toUpperCase()}`
 // }
+
+//Problem 6
+// document.addEventListener("DOMContentLoaded",() =>{
+
+// let submit = document.querySelector("#upperCaseBtn")
+
+// submit.addEventListener("click",() => {
+//     let str = document.querySelector("#str").value
+//     let num =0
+//     let upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+//     for(let i = 0; i < str.length; i++){
+//         if(upper.includes(str[i])) num++ 
+//     }
+//     document.querySelector("#result").innerText =  num
+// })
+
+
+// })
+
+
+document.addEventListener("DOMContentLoaded",() =>{
+
+const numUpper = (string) =>{
+    let num =0
+    let upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    for(let i = 0; i < string.length; i++){
+        if(upper.includes(string[i])) num++ 
+    }
+    return num
+
+}
+
+let submit = document.querySelector("#upperCaseBtn")
+
+submit.addEventListener("click",() => {
+    let stringone = document.querySelector("#strone").value
+    let stringtwo = document.querySelector("#strtwo").value
+    if(numUpper(stringone) > numUpper(stringtwo)){
+        return  document.querySelector("#result").innerText = "First string has more capital letters"
+    } else if (numUpper(stringone) < numUpper(stringtwo)) {
+        return document.querySelector("#result").innerText = "Second string has more capital letters"
+    } else {
+        return document.querySelector("#result").innerText = "Both strings have equal amounts of capital letters"
+    }
+
+})
+    
+
+
+
+})
