dc41dbf5dee80375e8457fcad4fe5f18451baa14
Samantha Jimenez <samanthajimenez@pursuit.org>
2019-11-09 14:27:33 -0500

    complete problem 2

--- htmlJavaScriptLab.js
+++ htmlJavaScriptLab.js
@@ -1,6 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-            let newString = prompt("Please enter string")
-            alert(newString);
-            let popText = document.querySelector("#top")
-            popText.innerText = newString
+        //          QUESTION 1
+            // let newString = prompt("Please enter string")
+            // alert(newString);
+            // let popText = document.querySelector("#top")
+            // popText.innerText = newString
+
+            //       QUESTION 2
+            // let userNum = prompt("Enter a number:");
+            // if(userNum % 2 === 0) {
+            //     alert("This number is even.")
+            // } else {
+            //     alert("This number is odd.")
+            // }
+            // let numText = document.querySelector("#num");
+            // numText.innerText = userNum;
 })
