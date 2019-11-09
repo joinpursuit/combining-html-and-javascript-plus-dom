beeed546ffec59ad5d0a3536f7bfd87d07c3a447
Kevin Wong <kevinwong@pursuit.org>
2019-11-09 14:48:43 -0500

    Question 1 & 2 completed

---
+++ index.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    // let q1 = prompt("Enter a string:")
+    // // alert(q1);
+    // let header = document.querySelector("#q1")
+    // header.innerText = q1;
+
+
+
+    // let q2 = prompt("Enter a number:")
+    // let header2 = document.querySelector("#q2");
+    // if(q2 % 2 === 0){
+    //     // alert("Number is EVEN");
+    //     header2.innerText = "Number is EVEN";
+    // } else {
+    //     // alert("Number is NOT EVEN");
+    //     header2.innerText = "Number is NOT EVEN";
+    // }
+
+    let number = document.getElementById("#q3");
+    if(document.querySelector("#x2")){
+        
+    }
+    
+    
+    debugger
+
+})
