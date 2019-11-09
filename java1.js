502f64ac79584cbbf0a877c26e8c70a29cd09482
darsuabasi <uduakabasiabasiurua@pursuit.org>
2019-11-09 16:19:30 -0500

    finished q2

--- java1.js
+++ java1.js
@@ -1,9 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let string = prompt("What is your name?") 
-    let pop = alert(`${string}`)
-    document.querySelector("#q1b-string").innerText = innerText = string
-
-})
-
+// document.addEventListener("DOMContentLoaded", () => {
+//     let string = prompt("What is your name?") 
+//     let pop = alert(`${string}`)
+//     document.querySelector("#q1b-string").innerText = innerText = string
+// })
 
 
+// document.addEventListener("DOMContentLoaded", () => {
+
+// let numbers = prompt("Enter a number")
+// let result = (`${num}`)
+
+// const isEven = () => {
+//     if(num % 2 === 0) {
+//         return num + " is an even number"
+//     } else {
+//         return num + " is an odd number"
+//     }
+//     let pop = alert(`${num}`)
+// document.querySelector("#Q2string").innerText = innerText = string
+// }
+// })
