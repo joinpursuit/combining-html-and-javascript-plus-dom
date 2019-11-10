40c59baebc0ca9afdd226dc59dee44fda4e860cb
david maravillas <davidmaravillas@pursuit.org>
2019-11-10 14:06:10 -0500

    Q8 complete

--- index.js
+++ index.js
@@ -1,55 +1,35 @@
-const capCounterComp = () =>{
-  let strElement1 = document.querySelector("#string1")
-  let strElement2 = document.querySelector("#string2")
-  let str1 = strElement1.value
-  let str2 = strElement2.value
-
-const count = (str)=>{
-      let count = 0
-      for(i =0; i <str.length; i++){
-        if(str[i] === str[i].toUpperCase()){
-          count += 1
-        }
-      }
-      return count
+const avr = ()=>{
+  let numEl1 = document.querySelector("#number1")
+  let numEl2 = document.querySelector("#number2")
+  let numEl3 = document.querySelector("#number3")
+  let num1 = Number(numEl1.value)
+  let num2 = Number(numEl2.value)
+  let num3 = Number(numEl3.value)
+  let average = (num1 + num2 + num3)/ 3;
+      alert(`The average of the three num is ${average}`)
+      //newSent.innerText = `${str1} has more capital letters`
+}
+const avr2 = ()=>{
+  let numEl1 = document.querySelector("#number1")
+  let numEl2 = document.querySelector("#number2")
+  let numEl3 = document.querySelector("#number3")
+  let newAvr = document.querySelector("#surprise")
+  let num1 = Number(numEl1.value)
+  let num2 = Number(numEl2.value)
+  let num3 = Number(numEl3.value)
+  let average = (num1 + num2 + num3)/ 3;
+  
+  if(isNaN(average)){
+    alert("All of the inputs must be numbers")
+  } else {
+    newAvr.innerText = `The average of the three num is ${average}`
+  }
+  debugger
+      //newSent.innerText = `${str1} has more capital letters`
 }
 
-if(count(str1) > count(str2)){
-  alert(`${str1} has more capital letters`)
-} else if (count(str1) < count(str2)){
-  alert(`${str2} has more capital letters`)
-} else{
-  alert(`both are equal`)
-}
-}
-
-const capCounterComp1 = () =>{
-  let strElement1 = document.querySelector("#string1")
-  let strElement2 = document.querySelector("#string2")
-  let newSent = document.querySelector("#surprise")
-  let str1 = strElement1.value
-  let str2 = strElement2.value
-
-const count = (str)=>{
-      let count = 0
-      for(i =0; i <str.length; i++){
-        if(str[i] === str[i].toUpperCase()){
-          count += 1
-        }
-      }
-      return count
-}
-
-if(count(str1) > count(str2)){
-  newSent.innerText = `${str1} has more capital letters`
-} else if (count(str1) < count(str2)){
-  newSent.innerText = `${str2} has more capital letters`
-} else{
-  newSent.innerText = `both are equal`
-}
-}
 
 document.addEventListener("DOMContentLoaded", () =>{
   let submitButton = document.querySelector("#submitButton")
-  submitButton.addEventListener("click", capCounterComp1)
+  submitButton.addEventListener("click", avr2)
 })
