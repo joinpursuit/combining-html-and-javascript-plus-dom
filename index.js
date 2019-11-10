869a086cfd0f6d52064a7fe3095eeea949b7fea1
david maravillas <davidmaravillas@pursuit.org>
2019-11-10 12:11:28 -0500

    Q7 complete

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
-// const toUpper1 = () =>{
-//   let strElement = document.querySelector("#string")
-//   let str = strElement.value
-//   alert(str.toUpperCase())
-// }
-// const toUpper2 = () =>{
-//   let strElement = document.querySelector("#string")
-//   let str = strElement.value
-//   strElement.value = str.toUpperCase()
-// }
-// const toUpper3 = () =>{
-//   let strElement = document.querySelector("#string")
-//   let newSent = document.querySelector("#surprise")
-//   let str = strElement.value
-//   newSent.innerText = str.toUpperCase()
-// }
+const capCounterComp = () =>{
+  let strElement1 = document.querySelector("#string1")
+  let strElement2 = document.querySelector("#string2")
+  let str1 = strElement1.value
+  let str2 = strElement2.value
 
-const capCounter = () =>{
-  let strElement = document.querySelector("#string")
-  let newSent = document.querySelector("#surprise")
-  let str = strElement.value
-  let count = 0
-  for(i =0; i <str.length; i++){
-    if(str[i] === str[i].toUpperCase()){
-      count += 1
-    }
-  }
-  newSent.innerText = `you have ${count} capital letters`
+const count = (str)=>{
+      let count = 0
+      for(i =0; i <str.length; i++){
+        if(str[i] === str[i].toUpperCase()){
+          count += 1
+        }
+      }
+      return count
 }
 
-const capCounter2 = () =>{
-  let strElement = document.querySelector("#string")
-  let str = strElement.value
-  let count = 0
-  for(i =0; i <str.length; i++){
-    if(str[i] === str[i].toUpperCase()){
-      count += 1
-    }
-  }
-  alert(`you have ${count}`)
+if(count(str1) > count(str2)){
+  alert(`${str1} has more capital letters`)
+} else if (count(str1) < count(str2)){
+  alert(`${str2} has more capital letters`)
+} else{
+  alert(`both are equal`)
+}
+}
+
+const capCounterComp1 = () =>{
+  let strElement1 = document.querySelector("#string1")
+  let strElement2 = document.querySelector("#string2")
+  let newSent = document.querySelector("#surprise")
+  let str1 = strElement1.value
+  let str2 = strElement2.value
+
+const count = (str)=>{
+      let count = 0
+      for(i =0; i <str.length; i++){
+        if(str[i] === str[i].toUpperCase()){
+          count += 1
+        }
+      }
+      return count
+}
+
+if(count(str1) > count(str2)){
+  newSent.innerText = `${str1} has more capital letters`
+} else if (count(str1) < count(str2)){
+  newSent.innerText = `${str2} has more capital letters`
+} else{
+  newSent.innerText = `both are equal`
+}
 }
 
 document.addEventListener("DOMContentLoaded", () =>{
   let submitButton = document.querySelector("#submitButton")
-  submitButton.addEventListener("click", capCounter)
+  submitButton.addEventListener("click", capCounterComp1)
 })
