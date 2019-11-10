ab275f2742b9a529816200629dff14328fab9c7b
dannyl <danilovlaurent@pursuit.org>
2019-11-10 12:55:24 -0500

    problem3

--- DomLab.js
+++ DomLab.js
@@ -1,20 +1,10 @@
- 
-
-document.addEventListener("DOMContentLoaded",() => {
-
-    document.getElementById("submit").addEventListener("click",() => { 
-        let element = document.getElementById("input_number").value * 2
-        //console.log(element)
-        // return element * 2  
-        alert(element)
-    }) 
- }) 
-
-
-    
+ // QUESTION 1  
 
 // let str = prompt("Enter your name to continue")
 // alert(`All right ${str}, you are about to receive street knowledge`)
+
+
+//QUESTION 2
 
 // let evenNum = prompt("enter your age")
 // if(evenNum %2 === 0){
@@ -28,3 +18,46 @@ document.addEventListener("DOMContentLoaded",() => {
 //  let input_number = (num) => {
 //      alert(num * 2)
 //  }
+
+
+// Question 3
+// document.addEventListener("DOMContentLoaded",() => {
+
+//     document.getElementById("submit").addEventListener("click",() => { 
+//         let element = document.getElementById("input_number").value * 2
+//         //console.log(element)
+//         // return element * 2  
+//         alert(element)
+//     }) 
+//  }) 
+
+//QUESTION 3B
+
+
+// document.addEventListener("DOMContentLoaded",() => {
+
+//     document.getElementById("submit").addEventListener("click",() => { 
+//         let element = document.getElementById("input_number").value * 2
+//         document.getElementById("doubled").innerText = element
+//     }) 
+//  }) 
+
+//  QUESTION 3C
+
+document.addEventListener("DOMContentLoaded",() => {
+
+    document.getElementById("submit").addEventListener("click",() => { 
+        let element = document.getElementById("input_number")
+        let output = element.value * 2
+        element.value = output
+        
+        
+        
+        // debugger
+    }) 
+ }) 
+
+
+
+
+
