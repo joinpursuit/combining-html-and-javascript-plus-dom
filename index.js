1bc0a4415530ef1ce7b65fbdf3512fe579c00697
david maravillas <davidmaravillas@pursuit.org>
2019-11-10 14:15:40 -0500

    Q9 complete

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-const avr = ()=>{
+const CheckMid1 = ()=>{
   let numEl1 = document.querySelector("#number1")
   let numEl2 = document.querySelector("#number2")
   let numEl3 = document.querySelector("#number3")
   let num1 = Number(numEl1.value)
   let num2 = Number(numEl2.value)
   let num3 = Number(numEl3.value)
-  let average = (num1 + num2 + num3)/ 3;
-      alert(`The average of the three num is ${average}`)
-      //newSent.innerText = `${str1} has more capital letters`
-}
-const avr2 = ()=>{
-  let numEl1 = document.querySelector("#number1")
-  let numEl2 = document.querySelector("#number2")
-  let numEl3 = document.querySelector("#number3")
-  let newAvr = document.querySelector("#surprise")
-  let num1 = Number(numEl1.value)
-  let num2 = Number(numEl2.value)
-  let num3 = Number(numEl3.value)
-  let average = (num1 + num2 + num3)/ 3;
-  
-  if(isNaN(average)){
-    alert("All of the inputs must be numbers")
+  if(num2 === num1+num3){
+    alert(true)
   } else {
-    newAvr.innerText = `The average of the three num is ${average}`
+    alert(false)
   }
-  debugger
-      //newSent.innerText = `${str1} has more capital letters`
 }
 
+const CheckMid2 = ()=>{
+  let numEl1 = document.querySelector("#number1")
+  let numEl2 = document.querySelector("#number2")
+  let numEl3 = document.querySelector("#number3")
+  let newSent = document.querySelector("#surprise")
+
+  let num1 = Number(numEl1.value)
+  let num2 = Number(numEl2.value)
+  let num3 = Number(numEl3.value)
+  if(num2 === num1+num3){
+    newSent.innerText = `true`
+  } else {
+    newSent.innerText = `false`
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () =>{
   let submitButton = document.querySelector("#submitButton")
-  submitButton.addEventListener("click", avr2)
+  submitButton.addEventListener("click", CheckMid2)
 })
