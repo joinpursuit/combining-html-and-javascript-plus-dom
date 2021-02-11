b73265624b8acaa9c27f317de7d60569e8f2c45b
Jeffrey <jeffreysebastian@pursuit.org>
2021-02-11 14:38:00 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -17,3 +17,32 @@ const palindrome = () => {
     let answer = arr.every((el, i) => el === input[input.length -1 -i]);
     p.innerText = `It is ${answer} that ${input} is a palindrome`;
     }
+
+const isEven = () => {
+    const input = document.getElementById("even-checker-input").value;
+    const p = document.getElementById("even-checker-output");
+    let answer = input % 2 === 0;
+    p.innerText = `It is ${answer} that ${input} is even`;
+}
+
+const numDoubler = () => {
+    const input = Number(document.getElementById("doubler-input").value);
+    const p = document.getElementById("doubler-output");
+    let answer = input * 2
+    p.innerText = `${input} doubled is ${answer}`
+}
+
+const averageOfThreeNums = () => {
+    const input1 = Number(document.getElementById("average-input-1").value);
+    const input2 = Number(document.getElementById("average-input-2").value);
+    const input3 = Number(document.getElementById("average-input-3").value);
+    const p = document.getElementById("average-output");
+    let answer = (input1 + input2 + input3) / 3;
+    p.innerText = `The average of ${input1}, ${input2}, and ${input3} is ${answer}`
+}
+
+const vowelRemover = () => {
+    const input = document.getElementById("vowel-remover-input").value;
+    
+}
+   
