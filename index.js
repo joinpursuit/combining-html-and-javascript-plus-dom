134c2e4f229080782b9619865063c7bd463b427d
itsanasna <anasnahil838@gmail.com>
2021-02-10 22:00:49 -0500

    finished problem 1 and 2, some of 3

---
+++ index.js
@@ -0,0 +1,20 @@
+const stringMirror = () => {
+    const input = document.getElementById("mirror-input");
+    const p = document.getElementById("mirror-output");
+    p.innerText = input.value;
+}
+
+const stringUpperCase = () => {
+    const input = document.getElementById("uppercaser-input").value.toUpperCase();
+    const p = document.getElementById("uppercaser-output")
+    p.innerText = input
+}
+
+const palindromeDetector = () =>{
+    const input = document.getElementById("palindrome-input").value
+    const p = document.getElementById("palindrome-output")
+    for(let i = 0; i < value ; i++) {
+        if (value[i] == value[value.length - 1])
+
+    }
+}
