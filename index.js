648d1d97facfa5aca1990d61b2f00e0f75d0a2ca
anjuhypp <anjuhypp@gmail.com>
2021-02-11 22:30:49 -0500

    did palindrome

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
 const displayString = () => {
-    const input = document.querySelector("#mirror-input").value;
-    const output = document.querySelector("#mirror-output");
-    output.innerText = `${input}`;
-}
+  const input = document.querySelector("#mirror-input").value;
+  const output = document.querySelector("#mirror-output");
+  output.innerText = `${input}`;
+};
 
 const displayUppercase = () => {
-    const input = document.querySelector("#uppercaser-input").value;
-    const output = document.querySelector("#uppercaser-output");
-    output.innerText = `${input.toUpperCase()}`;
-}
+  const input = document.querySelector("#uppercaser-input").value;
+  const output = document.querySelector("#uppercaser-output");
+  output.innerText = `${input.toUpperCase()}`;
+};
 
 const displayPalindrome = () => {
-    const input = document.querySelector("#palindrome-input").value;
-    const output = document.querySelector("#palindrome-output");
-    while (input.length > 0) {
-        slipt @ charachter
-        array.reverse
-        join
-        if join === input displayPalindrome
-        else, not
-    }
-    for ()
-    
-    output.innerText = `${input}`;
-}
-
-
-
-// const sumOfNum = () => {
-//     const num1 = Number(document.getElementById("num1").value);
-//     const num2 = Number(document.getElementById("num2").value);
-//     const p = document.getElementById("sum-equals");
-//     p.innerText = num1+ num2
-// }
+  const input = document.querySelector("#palindrome-input").value;
+  const output = document.querySelector("#palindrome-output");
+  let palindrome = input.split("").reverse().join("");
+  console.log(palindrome)
+  if (palindrome === input) {
+    output.innerText = `It is true that ${input} is a palindrome.`;
+  } else {
+    output.innerText = `It is false that ${input} is a palindrome.`;
+  }
+};
