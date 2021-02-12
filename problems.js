561b4a2ec6eec8acefa7f4a0a5dadcdfb1119743
Alexander <alexanderl96@yahoo.com>
2021-02-12 00:46:46 -0500

    2

--- problems.js
+++ problems.js
@@ -14,16 +14,48 @@ function upperCase(){
 function stringPanlinDrome(){
     const input =document.getElementById('palindrome-input')
     let text = input.value
-    let output = document.getElementById('palindrome-output')
+    const output = document.getElementById('palindrome-output')
     let string= ""
     for(let i = text.length-1;i >=0; i--){
         string+= text[i]
     }
-    if(text === string ){
+    if(text === string){
         output.textContent = `It is true that ${string} is a palindrome`
-
     }else{
         output.textContent = `It is false that ${string} is a palindrome`
     }
 
+}
+function eveNum(){
+    const input =document.getElementById('even-checker-input')
+    const text = input.value
+    const output = document.getElementById('even-checker-output')
+    const num = text
+    if(text % 2 === 0){
+        output.textContent =`It is true that ${num} is a palindrome`
+    }else{
+        output.textContent =`It is false that ${num} is a palindrome`
+    }
+
+}
+function doublerNum() {
+    const input =document.getElementById('doubler-input')
+    const text = input.value
+    const output = document.getElementById('doubler-output')
+    let doubler = text
+     if(text % doubler === 0){
+        output.textContent = `${text} doubled is ${doubler * 2}`
+     }
+}
+function averageTotal(){
+    const input =document.getElementById('average-input-2','average-input-1', 'average-input-3')
+    // const input =document.getElementById('average-input-1')
+    // const input =document.getElementById('average-input-3')
+    let text = input.value
+    const output = document.getElementById('average-output')
+    let average = text /3 
+   
+        output.textContent =`The average of ${text}, ${text}, and ${text} is ${average}`
+    
+    
 }
