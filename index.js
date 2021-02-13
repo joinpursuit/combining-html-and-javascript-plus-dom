22e8a9214be64048d8191f333519cebb604f84d5
Yashiry Azize <azizeyashiry@gmail.com>
2021-02-13 10:48:11 -0500

    completed even-checker, number doubler, & average

--- index.js
+++ index.js
@@ -17,3 +17,26 @@ const palindromeDetector = () =>{
     let answer = arr.every((el, i) => el === input[input.length -1 -i]);
     p.innerText = `It is ${answer} that ${input} is a palindrome`;
     }
+
+const evenChecker = () => {
+    const input = document.getElementById("even-checker-input").value
+    const p = document.getElementById("even-checker-output")
+    let answer = input % 2 === 0;
+    p.innerText= `It is ${answer} that ${input} is even`;
+    }
+
+const numberDoubler = () => {
+    const input = document.getElementById("doubler-input").value;
+    const p = document.getElementById("doubler-output");
+    let answer = input * 2;
+    p.innerText = `${input} doubled is ${answer}`;
+}
+
+const averageButton = () => {
+    const input1 = Number(document.getElementById("average-input-1").value);
+    const input2 = Number(document.getElementById("average-input-2").value);
+    const input3 = Number(document.getElementById("average-input-3").value);
+    const p = document.getElementById("average-output");
+    let average = (input1 + input2 + input3) / 3;
+    p.innerText = `The average of ${input1}, ${input2}, and ${input3} is ${average}`;
+}
