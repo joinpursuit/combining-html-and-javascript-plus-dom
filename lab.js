9eadbd3744cbff5b13a77fb963068b684ecbbee0
Obaid Rustemi <orustemi@gmail.com>
2021-02-13 11:21:43 -0500

    2nd commit

--- lab.js
+++ lab.js
@@ -1,5 +1,11 @@
 const mirrorStr = () => {
-    const inputTag = document.querySelector("#mirror-input");
-    const pTag = document.querySelector("#mirror-output");
-    pTag.innerText = inputTag.value
-}
+  const inputTag = document.querySelector("#mirror-input");
+  const pTag = document.querySelector("#mirror-output");
+  pTag.innerText = inputTag.value;
+};
+
+const stringUpperCaser = () => {
+  const upperCaserInput = document.querySelector("#uppercaser-input").value;
+  const upperCaserOutput = document.querySelector("#uppercaser-output");
+  upperCaserOutput.innerText = upperCaserInput.toUpperCase();
+};
