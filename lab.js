ba7c6e6c9a1dd87c86e7261c202babf99d844260
Coreen Cooper <coreencooper@pursuit.org>
2021-02-13 12:56:00 -0500

    palindromeDetector()

--- lab.js
+++ lab.js
@@ -9,3 +9,16 @@ const stringUpperCaser = () => {
   const upperCaserOutput = document.querySelector("#uppercaser-output");
   upperCaserOutput.innerText = upperCaserInput.toUpperCase();
 };
+
+const palindromeDetector = () => {
+  const userInput = document.querySelector("#palindrome-input").value;
+  let pTag = document.querySelector("#palindrome-output");
+  for (let i = 0; i < userInput.length ; i++) {
+    if (userInput[i] !== userInput.slice(-1 -i)[0]) {
+      pTag.innerText = `It is false that ${userInput} is a palindrome`
+      return
+    }
+  }
+  pTag.innerText = `It is true that ${userInput} is a palindrome`
+}
+
