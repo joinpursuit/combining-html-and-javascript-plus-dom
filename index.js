881971e785887e82da1e6bdedfd23b6a875a40d4
jessicanetto <75263227+jessicanetto@users.noreply.github.com>
2021-02-13 10:08:59 -0500

    3 complete questions

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ function stringMirror() {
 
 const stringUppercaser = () => {
   const input = document.querySelector("#uppercaser-input");
-  const p = document.querySelector("#uppercaser-output")
-  p.innerText = input.value.toUpperCase()
+  const p = document.querySelector("#uppercaser-output");
+  p.innerText = input.value.toUpperCase();
+};
+function palindromeDetector() {
+  let input = document.getElementById("palindrome-input");
+  let p = document.getElementById("palindrome-output");
+
+  let strArr = input.value.split("");
+
+  let arrReverse = strArr.reverse().join("");
+
+  if (input.value === arrReverse) {
+    p.innerText = `It is true that ${input.value} is a palindrome`;
+  } else if (strArr !== arrReverse) {
+    p.innerText = `It is false that ${input.value} is a palindrome`;
+  }
 }
