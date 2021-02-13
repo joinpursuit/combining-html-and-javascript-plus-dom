02e500e36e2fadcf91f0ab88cfd6732a31280fbf
Flower Aviles <itsfloweraviles@gmail.com>
2021-02-13 11:13:38 -0500

    AverageNums

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const replaceWithStar = () => {
-    const input = document.getElementById("replace-with-star");
-    const p = document.getElementById("text-with-star");
-    const str = input.value;
-    let output = "";
-    const vowels = "aeiouAEIOU";
-    for(const char of str) {
-        if(vowels.includes(char)) {
-            output += "*";
-        } else {
-            output += char; 
-        }
-    }
+// const replaceWithStar = () => {
+//     const input = document.getElementById("replace-with-star");
+//     const p = document.getElementById("text-with-star");
+//     const str = input.value;
+//     let output = "";
+//     const vowels = "aeiouAEIOU";
+//     for(const char of str) {
+//         if(vowels.includes(char)) {
+//             output += "*";
+//         } else {
+//             output += char; 
+//         }
+//     }
 
-    p.innerText = output; 
+//     p.innerText = output; 
+// }
+
+const averageNums = () => {
+    let num1 = Number(document.getElementById("average-input-1").value); 
+    let num2 = Number(document.getElementById("average-input-2").value);
+    let num3 = Number(document.getElementById("average-input-3").value); 
+    let p= document.getElementById("average-output");
+    let average = (num1 + num2 + num3) / 3 
+    p.innerText = `The average of ${num1}, ${num2}, and ${num3} is ${average}`
+   
+
 }
