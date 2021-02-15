6686c5cc585e2094bfbc5249cbeac6a692b7253f
Jenna Qian <jiayin1996@gmail.com>
2021-02-14 22:03:07 -0500

    String Mirror

---
+++ problems.js
@@ -0,0 +1,7 @@
+function stringMirror() {
+    const input= document.getElementById('mirror-input')
+    const text = input.value 
+    const output = document.getElementById('mirror-output')
+    output.textContent = text
+}
+
