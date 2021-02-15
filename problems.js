5225f777df3b7381b75448f0282ceb4563889ffd
Jenna Qian <jiayin1996@gmail.com>
2021-02-15 15:40:57 -0500

    Even Checker

--- problems.js
+++ problems.js
@@ -5,3 +5,60 @@ function stringMirror() {
     output.textContent = text
 }
 
+function uppercaser () {
+    const input = document.getElementById('uppercaser-input')
+    const text = input.value
+    const output = document.getElementById('uppercaser-output')
+    output.textContent = text.toUpperCase()
+}
+
+function palindrome() {
+    const input = document.getElementById('palindrome-input')
+    const text = input.value 
+    let output = document.getElementById('palindrome-output')
+    let newString = ""
+    for (let i = text.length -1; i >= 0; i--) {
+        newString = newString + text[i]
+    }
+    if(text === newString) {
+        output.textContent = `It is true that ${text} is a palindrome`
+    } else {
+        output.textContent = `It is false that ${text} is a palindrome`
+    }
+}
+
+function even() {
+    const input = document.getElementById('even-checker-input')
+    const text = input.value
+    const output = document.getElementById('even-checker-output')
+    if( text % 2 === 0) {
+        output.textContent = `It is true that ${text} is even`
+    } else {
+        output.textContent = `It is false that ${text} is even`
+    }
+}
+
+
+
+
+
+function bonus() {
+    // get text input DOM element 
+    const textInput = document.getElementById('vowel-remover-input')
+    // get the checkbox DOM element 
+    const checkbox= document.getElementById('y-is-vowel-checkbox').checked
+
+    //get the text from the input DOM element!
+    const text = textInput.value
+    // see if the checkbox is checked!
+    const isChecked = checkbox.checked
+
+    // remove all the vowels from text!
+
+    // Add the resulting string to p#vowel-remover-output
+
+}
+
+function removeVowels(text, yIsVowel) {
+    
+}
