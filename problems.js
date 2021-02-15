26ba1814ae87ff7aca6115ef08c6f2392c8c91b0
Tamzeed Rahman <tamzeedrahman@gmail.com>
2021-02-15 00:56:13 -0500

    got first two to finally pass

--- problems.js
+++ problems.js
@@ -1,13 +1,15 @@
-function stringMirror(){
-    document.getElementById("mirror-output").textContent = document.getElementById("mirror-input").value
-
-}
-
-
-stringUstringUppercaser()
-
-        let input = document.getElementById('uppercaser-input').value
-        let output = document.getElementById('uppercaser-output')
-        output.innerText = input.toUpperCase()
-      }
-}
+function stringMirror() {
+    // get input from DOM
+    const input = document.getElementById('mirror-input');
+    // get value from input 
+    const text = input.value
+    // get mirror output from DOM
+    const output = document.getElementById('mirror-output');
+    // change the textContent of mirror-output to match input value
+    output.textContent = text;
+  }
+  function stringUppercaser(){
+    let input = document.getElementById('uppercaser-input').value
+     let output = document.getElementById('uppercaser-output')
+     output.innerText = input.toUpperCase()
+  }
