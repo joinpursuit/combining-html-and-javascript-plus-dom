8aa1e45d8b7945e0b431b64b668ddc6e83499652
Obaid Rustemi <orustemi@gmail.com>
2021-02-15 14:23:23 -0500

    number 3 complete

--- lab.js
+++ lab.js
@@ -13,12 +13,22 @@ const stringUpperCaser = () => {
 const palindromeDetector = () => {
   const userInput = document.querySelector("#palindrome-input").value;
   let pTag = document.querySelector("#palindrome-output");
-  for (let i = 0; i < userInput.length ; i++) {
-    if (userInput[i] !== userInput.slice(-1 -i)[0]) {
-      pTag.innerText = `It is false that ${userInput} is a palindrome`
-      return
+  for (let i = 0; i < userInput.length; i++) {
+    if (userInput[i] !== userInput.slice(-1 - i)[0]) {
+      pTag.innerText = `It is false that ${userInput} is a palindrome`;
+      return;
     }
   }
-  pTag.innerText = `It is true that ${userInput} is a palindrome`
-}
+  pTag.innerText = `It is true that ${userInput} is a palindrome`;
+};
 
+const evenChecker = () => {
+  let userNum = document.getElementById("even-checker-input").value;
+  let isEven = document.getElementById("even-checker-output");
+
+  if (userNum % 2 === 0) {
+    isEven.innerText = `It is true that ${userNum} is even`;
+  } else {
+    isEven.innerText = `It is false that ${userNum} is even`;
+  }
+};
