530c6857030a8b945eb15a75f297bb24907b5ffe
Tamzeed Rahman <tamzeedrahman@gmail.com>
2021-02-14 22:40:24 -0500

    working on it

---
+++ problems.js
@@ -0,0 +1,13 @@
+function stringMirror(){
+    document.getElementById("mirror-output").textContent = document.getElementById("mirror-input").value
+
+}
+
+
+stringUstringUppercaser()
+
+        let input = document.getElementById('uppercaser-input').value
+        let output = document.getElementById('uppercaser-output')
+        output.innerText = input.toUpperCase()
+      }
+}
