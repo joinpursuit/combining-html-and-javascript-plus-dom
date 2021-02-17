977e23ec0b0ccbc30b3aa3003fee2be6c9897eff
Rayvon Finney Pinkston <rayfinn55@gmail.com>
2021-02-16 23:40:50 -0500

    domlab still kicking my butt

---
+++ problems.js
@@ -0,0 +1,21 @@
+function mirror() {
+const input = document.getElementById('mirror-input')
+
+const text = input.value 
+
+const output = document.getElementById('mirror-output')
+
+output.textContent = text
+
+}
+
+function upperCaser() {
+const strInput = document.getElementById('uppercaser-input')
+
+const upperCaseText = strInput.value
+
+const strOutput = document.getElementById('uppercase-output')
+
+strOutput.textContent = upperCaseText
+
+}
