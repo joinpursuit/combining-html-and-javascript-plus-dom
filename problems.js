63ef7715d12823ef7f286a84a9943c2ff41ebef7
Tamzeed Rahman <tamzeedrahman@gmail.com>
2021-02-19 16:13:49 -0500

    compeleted expect bonus

--- problems.js
+++ problems.js
@@ -13,21 +13,43 @@ function stringMirror() {
   let output = document.getElementById('uppercaser-output')
   output.innerText = input.toUpperCase()
   }
-
- function palindrome() {
-   
-  const input = document.getElementById("palindrome-input");
-  const string = input.value;
-  const output = document.getElementById("palindrome-output");
-  let newString = "";
-  for (let i = string.length - 1; i >= 0; i--) {
-      newString += string[i];
-  }
-  if (string === newString) {
+ 
+  function palindrome() {
+    const input = document.getElementById("palindrome-input");
+    let string = input.value;
+    let output = document.getElementById("palindrome-output");
+    let newStr = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+      newStr += string[i];
+    }
+    if (string === newStr) {
       output.textContent = `It is true that ${string} is a palindrome`;
-  } 
-  else {
+    } else {
       output.textContent = `It is false that ${string} is a palindrome`;
+    }
   }
 
+ function evenChecker(){
+  const input = document.getElementById("even-checker-input");
+  const output = document.getElementById("even-checker-output");
+  output.innerText = `It is ${input.value % 2 === 0} that ${input.value} is even`
+ }
+
+ function doubler() {
+    const input = document.getElementById("doubler-input");
+    const output = document.getElementById("doubler-output");
+    output.innerText = ` ${input.value} doubled is ${input.value * 2}`
+ }
+
+ function average() {
+    const input1 = document.getElementById("average-input-1");
+    const number1 = Number(input1.value)
+    const input2 = document.getElementById("average-input-2");
+    const number2 = Number(input2.value)
+    const input3 = document.getElementById("average-input-3");
+    const number3 = Number(input3.value)
+    
+    const average = (number1 + number2 + number3)/3
+    const output = document.getElementById("average-output");
+    output.innerText = `The average of ${number1}, ${number2}, and ${number3} is ${average}`
  }
