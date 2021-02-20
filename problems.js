130ec855d38bc366380296bf2d63e4ee70e21cb1
Talia Pichardo <taliapichardo@gmail.com>
2021-02-20 00:08:18 -0500

    Problem 1

---
+++ problems.js
@@ -0,0 +1,9 @@
+// Problem 1
+const stringMirror = () =>{
+    let input = document.getElementById("mirror-input")
+    let output = (document.getElementById("mirror-output"))
+    output.textContent = input.value
+    console.log(output.textContent)
+}
+// Problem 2
+// Problem 3
