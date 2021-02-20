3989fe4897187c7e4c9794e3e8c6b1c27bebf222
Talia Pichardo <taliapichardo@gmail.com>
2021-02-20 02:08:10 -0500

    Problem 3

--- problems.js
+++ problems.js
@@ -2,16 +2,30 @@ const stringMirror = () =>{
     let input = document.getElementById("mirror-input").value
     let output = (document.getElementById("mirror-output"))
     output.textContent = input
-}
+};
 
 const stringUppercaser = () =>{
     let input = document.getElementById("uppercaser-input").value
     let output = document.getElementById("uppercaser-output")
     output.textContent = input.toUpperCase()
-}
+};
 
 
 const PalindromeDetector = () =>{
+    let input = document.getElementById("palindrome-input").value
+    let output = document.getElementById("palindrome-output")
+    let str = String(input)
+    console.log(str)
+    let arr = []
+    for(let i = str.length -1; i >= 0; i--){
+        arr.push(str[i])
+    }
+    let joinedArr= arr.join("")
+    if(joinedArr === str){
+        output.textContent = `It is ${true} that ${input} is a palindrome `
+    }else{
+        output.textContent = `It is ${false} that ${input} is a palindrome `
+    }
 
 }
 
